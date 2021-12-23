46a8937ac5bf55e069f6b3a9fdb5e404067a4975
sushidesu <hugemerge@gmail.com>
2021-12-23 16:59:18 +0900

    remove unused files

--- src/make-pair.test.ts
+++
@@ -1,9 +0,0 @@
-import { make_pair } from "./make-pair"
-
-describe(`make_pair`, () => {
-  it(`start`, () => {
-    const result = make_pair([0, 1, 2, 3, 4])
-    console.log(result)
-    expect(1).toBe(1)
-  })
-})
